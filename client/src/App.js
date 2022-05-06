9b4cbe6123569e1a118c5615a17b29cdf2f108ef
Laura Del valle <l.delvallebermejo@gmail.com>
2022-05-06 13:22:57 +0200

    responsive navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import AppRoutes from './routes/AppRoutes';
+import Navigation from './components/Navigation/Navigation';
 
 
 function App() {
   return (
     <div>
-      
+       <Navigation/>
         
     </div>
   );
---
+++ client/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,43 @@
+import "./Navigation.css"
+import {  Nav } from 'react-bootstrap'
+import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faAlignJustify, faXmark} from '@fortawesome/free-solid-svg-icons'
+import { useState } from "react"
+
+const Navigation = () => {
+
+    const [click, setClick]= useState (false)
+
+    const handleClick = () => setClick(!click)
+
+    return (
+     <nav className="navBar">
+         <div className="nav-container">
+             <ul className={click ? 'Nav-menu active' : 'Nav-menu'}>
+               
+                <nav className={click ? 'Nav-menu active' : 'Nav-menu'}>
+
+                    <Link to="/Danza Okara" className="nav-item">
+                        <Nav.Link active className="NavLink" onClick={handleClick}> Restaurantes </Nav.Link>
+                    </Link>
+                    <Link to="/Actividades" className="nav-item">
+                        <Nav.Link active className="NavLink" onClick={handleClick}> Usuarios </Nav.Link>
+                    </Link>
+                    <Link to="/Proyecto" className="nav-item">
+                        <Nav.Link active className="NavLink" onClick={handleClick}> Grupos </Nav.Link>
+                    </Link>
+                    
+                </nav>
+             </ul>
+            <div className="Nav-Icon" onClick={handleClick}>
+                     <FontAwesomeIcon icon={ click? faXmark : faAlignJustify} />
+            </div>
+         </div>
+     </nav>
+
+    )
+
+}
+
+export default Navigation
