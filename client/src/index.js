e4f0bb865751e5c46ff4fe240edb1379934ede69
Laura Del valle <l.delvallebermejo@gmail.com>
2022-05-06 11:10:30 +0200

    first Commit

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from './App';
+import {createRoot} from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+createRoot(
+  document.getElementById('root')
+).render(
+  <Router>
+  <App/>
+  </Router>
+);
+ 
